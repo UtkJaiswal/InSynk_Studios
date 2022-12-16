ccf5c00705d80b1daa941a776c3509742e5d8ef1
Utkjaiswal <utkjaiswal58@gmail.com>
2022-12-16 13:58:10 +0530

    navbar setup done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar,Container,Nav,Form, FormControl,Button } from 'react-bootstrap';
 import React, { useState, useEffect } from 'react';
 
 function App() {
   //movies stores the movies to be displayed;
   const [movies, setMovies] = useState([]);
+
+  //for searching movies
+  const [query, setQuery]=useState('');
+
+
+  //for storing total number of movies 
+  const [totalResults,setTotalResults]=useState(0);
+
 
 
   //page stores the page number to furthur fetch api requests
@@ -51,10 +61,54 @@ function App() {
     fetchData();
 
   },[])
+
+  //function to search movie
+  const searchMovie = async(e)=>{
+    e.preventDefault();
+    console.log("Searching");
+    try{
+      const url=API_URL+"?api_key="+API_KEY+"&query="+query+"&sort_by=primary_release_date.desc"
+      // const url=`https://api.themoviedb.org/3/search/movie?api_key=1b7cbfe3c55ec3b24cb5d15d9c16a490&query=${query}&sort_by=primary_release_date.desc`;
+      const res= await fetch(url);
+      const data= await res.json();
+      console.log(data);
+      setMovies(data.results);
+    }
+    catch(e){
+      console.log(e);
+    }
+  }
+  //handle change
+  const changeHandler=(e)=>{
+    setQuery(e.target.value);
+  }
   return (
-    <div>
-      {/* {process.env.REACT_APP_API_URL} */}
-    </div>
+    <>
+      <Navbar bg="dark" expand="lg" variant="dark">
+      <Container fluid>
+        <Navbar.Brand href="/home">INSYNC <br/>STUDIOS</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll"></Navbar.Toggle>
+
+          <Navbar.Collapse id="nabarScroll">
+            <Nav 
+            className="me-auto my-2 my-lg-3"
+            style={{maxHeight:'100px'}}
+            navbarScroll></Nav>
+
+            <Form className="d-flex" onSubmit={searchMovie} autoComplete="off">
+              <FormControl
+              type="search"
+              placeholder="Search for a movie"
+              className="me-2"
+              aria-label="search"
+              name="query"
+              value={query} onChange={changeHandler}></FormControl>
+              {/* <Button variant="secondary" type="submit">Search</Button> */}
+            </Form>
+          </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </>
   );
 }
 
