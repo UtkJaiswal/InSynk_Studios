0e9028a90a3a72c44a08fddeba0bb3e1da3de729
Utkjaiswal <utkjaiswal58@gmail.com>
2022-12-16 15:08:26 +0530

    infinite scrolling feacture added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Navbar,Container,Nav,Form, FormControl,Button } from 'react-bootstrap';
+import { Navbar, Container, Nav, Form, FormControl, Button } from 'react-bootstrap';
 import React, { useState, useEffect } from 'react';
 import MovieModal from './Components/MovieModal';
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 function App() {
   //movies stores the movies to be displayed;
   const [movies, setMovies] = useState([]);
 
   //for searching movies
-  const [query, setQuery]=useState('');
+  const [query, setQuery] = useState('');
 
 
   //for storing total number of movies 
-  const [totalResults,setTotalResults]=useState(0);
+  const [totalResults, setTotalResults] = useState(0);
 
 
 
@@ -28,7 +29,7 @@ function App() {
   const API_KEY = process.env.REACT_APP_API_KEY;
 
 
-  const API_SEARCH=process.env.REACT_APP_API_SEARCH;
+  const API_SEARCH = process.env.REACT_APP_API_SEARCH;
 
 
   //url1 is the url to fetch the list of movies
@@ -40,13 +41,13 @@ function App() {
   //useEffect is needed to fetch the list of movies when the user first enters the site
   useEffect(() => {
     //fetchData function is used for API calling
-    async function fetchData(){
+    async function fetchData() {
       const res = await fetch(url1)
 
       //if movies exists
       if (res.status === 200) {
         // console.log("he;l")
-        const data=await res.json();
+        const data = await res.json();
         setMovies(movies.concat(data.results))
       }
 
@@ -65,78 +66,87 @@ function App() {
     }
     fetchData();
 
-  },[])
+  }, [])
 
   //function to fetch more movie on scrolling
   const fetchMoreData = () => {
-    setPage(page+1);
-    fetch(API_URL)
-    .then((res)=>res.json())
-    .then(data=>{
-      // console.log(data);
-      // setTotalResults(data.total_results)
-      setMovies(movies.concat(data.results));
-    })
+    setPage(page + 1);
+    fetch(url1)
+      .then((res) => res.json())
+      .then(data => {
+        // console.log(data);
+        // setTotalResults(data.total_results)
+        setMovies(movies.concat(data.results));
+      })
   };
 
   //function to search movie
-  const searchMovie = async(e)=>{
+  const searchMovie = async (e) => {
     e.preventDefault();
-    console.log("Searching",query);
-    try{
-      const url=API_SEARCH+"/?api_key="+API_KEY+"&query="+query+"&sort_by=primary_release_date.desc"
+    console.log("Searching", query);
+    try {
+      const url = API_SEARCH + "/?api_key=" + API_KEY + "&query=" + query + "&sort_by=primary_release_date.desc"
       // const url=`https://api.themoviedb.org/3/search/movie?api_key=1b7cbfe3c55ec3b24cb5d15d9c16a490&query=${query}&sort_by=primary_release_date.desc`;
-      const res= await fetch(url);
-      const data= await res.json();
-      console.log("Searching movie is going on",data);
+      const res = await fetch(url);
+      const data = await res.json();
+      console.log("Searching movie is going on", data);
       setMovies(data.results);
     }
-    catch(e){
+    catch (e) {
       console.log(e);
     }
   }
   //handle change
-  const changeHandler=(e)=>{
+  const changeHandler = (e) => {
     setQuery(e.target.value);
   }
   return (
     <>
       <Navbar bg="dark" expand="lg" variant="dark">
-      <Container fluid>
-        <Navbar.Brand href="/home">INSYNC <br/>STUDIOS</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll"></Navbar.Toggle>
+        <Container fluid>
+          <Navbar.Brand href="/home">INSYNC <br />STUDIOS</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll"></Navbar.Toggle>
 
           <Navbar.Collapse id="nabarScroll">
-            <Nav 
-            className="me-auto my-2 my-lg-3"
-            style={{maxHeight:'100px'}}
-            navbarScroll></Nav>
+            <Nav
+              className="me-auto my-2 my-lg-3"
+              style={{ maxHeight: '100px' }}
+              navbarScroll></Nav>
 
             <Form className="d-flex" onSubmit={searchMovie} autoComplete="off">
               <FormControl
-              type="search"
-              placeholder="Search for a movie"
-              className="me-2"
-              aria-label="search"
-              name="query"
-              value={query} onChange={changeHandler}></FormControl>
+                type="search"
+                placeholder="Search for a movie"
+                className="me-2"
+                aria-label="search"
+                name="query"
+                value={query} onChange={changeHandler}></FormControl>
               {/* <Button variant="secondary" type="submit">Search</Button> */}
             </Form>
           </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    <div>
-      {movies.length > 0 ?(
-        <div className="container">
-        <div className="grid">
-          {movies.map((movie)=>
-          <MovieModal key={movie.id+Math.random(10)} {...movie}/>)}
+        </Container>
+      </Navbar>
+
+
+      <InfiniteScroll
+        dataLength={movies.length}
+        next={fetchMoreData}
+        hasMore={movies.length !== totalResults}
+        loader={<h4>Loading...</h4>}
+      >
+        <div>
+          {movies.length > 0 ? (
+            <div className="container">
+              <div className="grid">
+                {movies.map((movie) =>
+                  <MovieModal key={movie.id + Math.random(10)} {...movie} />)}
+              </div>
             </div>
-    </div>
-      ):(
-        <h2>No Movies Found</h2>
-      )}
-    </div>  
+          ) : (
+            <h2>No Movies Found</h2>
+          )}
+        </div>
+      </InfiniteScroll>
     </>
   );
 }
--- src/Components/MovieModal.js
+++ src/Components/MovieModal.js
@@ -1,46 +1,55 @@
-import { Modal,Button} from 'react-bootstrap';
-import React, {useEffect, useState} from 'react';
-const API_IMG=process.env.REACT_APP_API_IMG;
+import { Modal, Button } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+const API_IMG = process.env.REACT_APP_API_IMG;
 
-const MovieModal =({title, poster_path, vote_average, release_date, overview})=>{
-    
-    const [show, setShow]=useState(false);
+const MovieModal = ({ title, poster_path, vote_average, release_date, overview }) => {
 
-    const [imgurl,setImgurl]=useState("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwSdpOfQ69r4j_GBaK06LVCCWCz2_a6xDtb90ySZ6fHb54AyBY-skdl0ji3cULkUQmYx4&usqp=CAU")
 
-    const handleShow=()=>setShow(true);
-    const handleClose=()=>setShow(false);
+    //to check whether or not to show the modal
+    const [show, setShow] = useState(false);
 
-    useEffect(()=>{
-        if(poster_path!=null)
-        setImgurl(API_IMG+poster_path)
+    //if image exists for that movie fine else a default url needs to be given to show a defualt image
+    const [imgurl, setImgurl] = useState("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwSdpOfQ69r4j_GBaK06LVCCWCz2_a6xDtb90ySZ6fHb54AyBY-skdl0ji3cULkUQmYx4&usqp=CAU")
 
-    },[poster_path])
-    
-    return(
+
+    //function to manage and show the modal on clicking View More..
+    const handleShow = () => setShow(true);
+
+    //function to manage and hide the modal on closing the modal
+    const handleClose = () => setShow(false);
+
+
+    //useEffect to check whenever a url path is correct change it from default to that image path
+    useEffect(() => {
+        if (poster_path != null)
+            setImgurl(API_IMG + poster_path)
+
+    }, [poster_path])
+
+    return (
         <div className="card text-center bg-secondary mb-3">
             <div className="card-body">
-              <img className="card-img-top" src={imgurl} alt={title}/>
-              <div className="card-body">
-                  <button type="button" className="btn btn-dark" onClick={handleShow} >View More</button>
-                  <Modal show={show} onHide={handleClose}>
-                      <Modal.Header closeButton>
-                        <Modal.Title></Modal.Title>
-                      </Modal.Header>
-                      <Modal.Body>
-                      <img className="card-img-top" style={{width:'14rem',height:'auto'}}src={API_IMG+poster_path} alt={title}/>
-                      <h3>{title}</h3>
-                      <h4>IMDb: {vote_average}</h4>
-                      <h5>Release Date: {release_date}</h5>
-                      <br></br>
-                      <h6>Overview</h6>
-                      <p>{overview}</p>
-                      </Modal.Body>
-                      <Modal.Footer>
-                          <Button variant="secondary" onClick={handleClose}>Close</Button>
-                      </Modal.Footer>
-                  </Modal>
-              </div>
+                <img className="card-img-top" src={imgurl} alt={title} />
+                <div className="card-body">
+                    <button type="button" className="btn btn-dark" onClick={handleShow} >View More</button>
+                    <Modal show={show} onHide={handleClose}>
+                        <Modal.Header closeButton>
+                            <Modal.Title></Modal.Title>
+                        </Modal.Header>
+                        <Modal.Body>
+                            <img className="card-img-top" style={{ width: '14rem', height: 'auto' }} src={API_IMG + poster_path} alt={title} />
+                            <h3>{title}</h3>
+                            <h4>IMDb: {vote_average}</h4>
+                            <h5>Release Date: {release_date}</h5>
+                            <br></br>
+                            <h6>Overview</h6>
+                            <p>{overview}</p>
+                        </Modal.Body>
+                        <Modal.Footer>
+                            <Button variant="secondary" onClick={handleClose}>Close</Button>
+                        </Modal.Footer>
+                    </Modal>
+                </div>
             </div>
         </div>
     )
